8ce9ef4d1f380c082070b78a315a5263dd8a34ac
HezerSantos <HezerSantos24@gmail.com>
2025-04-04 19:42:41 -0500

    Finished the message page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,31 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import '../assets/styles/Dashboard.css'
 import DashboardNav from '../components/dashboard/DashboardNav'
+import { IoMdSend } from "react-icons/io";
+import MessageBlock from '../components/dashboard/MessageBlock';
+
+const handleInput = (e, setInput) => {
+    setInput(e.target.value)
+}
 const Dashboard = () => {
+    const [ message, setMessage ] = useState("")
     const dashboardMain = useRef(null)
     return(
         <>
             <main className='dashboard__main' ref={dashboardMain}>
                 <DashboardNav dashboardMain={dashboardMain}/>
                 <section className='dashboard__messages'>
-                    <input type="text" />
+                    <form>
+                        <textarea 
+                            name="" 
+                            id=""
+                            onChange={(e) => handleInput(e, setMessage)}
+                            value={message}  
+                            className='dashboard__input'
+                        ></textarea>
+                        <button><IoMdSend /></button>
+                    </form>
+                    <MessageBlock />
                 </section>
             </main>
         </>
