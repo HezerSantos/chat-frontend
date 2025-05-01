a4dcf7477c82c3152346487602d941d66244047a
HezerSantos <HezerSantos24@gmail.com>
2025-05-01 18:12:59 -0500

    prod v1.0.1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   production: {
-    apiUrl: import.meta.env.API_URL,
+    apiUrl: 'lunarlink-api.hallowedvisions.com',
   },
   development: {
     apiUrl: 'http://localhost:8080',
