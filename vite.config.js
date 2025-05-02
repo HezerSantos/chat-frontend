5847f73f185a13b607c09c05ec2f3c92c307b21b
HezerSantos <HezerSantos24@gmail.com>
2025-05-01 19:30:21 -0500

    prod v1.0.6

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this is set to the correct base path (e.g., if deploying to /subpath, set this to '/subpath/')
   test: {
     globals: true,
     environment: 'jsdom',
