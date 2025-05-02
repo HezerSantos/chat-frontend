5402ceb0ce0d367b8979fdb84bd669e87ca9d390
HezerSantos <HezerSantos24@gmail.com>
2025-05-01 19:44:53 -0500

    prod v1.0.8

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is set to the correct base path (e.g., if deploying to /subpath, set this to '/subpath/')
+  base: '/', // Ensure this is set to the correct base path
+  build: {
+    outDir: 'dist', // Ensure the build output directory is 'dist'
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
+  },
+  server: {
+    proxy: {
+      '/api': 'http://lunarlink-api.hallowedvisions.com', // Assuming your API is hosted separately
+    },
   },
 })
